a51ced5a05122f4f13129aaf55a7a2d6ad8dee3d
Pieter Vogelaar <pieter@pietervogelaar.nl>
2011-09-15 15:31:06 +0000

    PHP error logging is optional now and file permissions can be defined

--- Application/Resource/Error.php
+++ Application/Resource/Error.php
@@ -28,6 +28,28 @@
 /**
  * Sets the PHP error handler and logs PHP errors
  *
+ * To bootstrap this application resource add the following to your application.ini:
+ *
+ * pluginPaths.Pike_Application_Resource = "Pike/Application/Resource"
+ * autoloaderNamespaces[] = "Pike"
+ * resources.error.errorHandler =
+ *
+ * If you want to log PHP errors add the following to your application.ini:
+ *
+ * resources.log.php.writerName = "Stream"
+ * resources.log.php.writerParams.stream = APPLICATION_PATH "/../data/logs/php_errors.log"
+ * resources.log.php.writerParams.mode = "a"
+ * resources.log.php.writerParams.permission = "666"
+ * resources.log.php.filterName = "Priority"
+ * resources.log.php.filterParams.priority = 7
+ * resources.log.php.formatterName = "Simple"
+ * resources.log.php.formatterParams.format = "%timestamp% %priorityName% (%priority%): %message% on line %line% in file %file%" PHP_EOL
+ *
+ * If you want to convert PHP errors to exceptions set a value for "resources.error.errorHandler"
+ * in your application.ini:
+ *
+ * resources.error.errorHandler = "Pike_ErrorHandler"
+ *
  * @link       http://php.net/manual/en/function.set-error-handler.php
  * @category   PiKe
  * @copyright  Copyright (C) 2011 by Pieter Vogelaar (platinadesigns.nl) and Kees Schepers (keesschepers.nl)
@@ -37,12 +59,24 @@ class Pike_Application_Resource_Error extends Zend_Application_Resource_Resource
 {
     public function init()
     {
-        $options = $this->getOptions();
-        $handler = isset($options['handler']) ? $options['handler'] : 'Pike_ErrorHandler';
-        set_error_handler(array($handler, 'dispatch'));
+        $configErrorHandler = Zend_Registry::get('config')->resources->error->errorHandler;
+        if (isset($configErrorHandler) && $configErrorHandler != '') {
+            $options = $this->getOptions();
+            $handler = isset($options['handler']) ? $options['handler'] : 'Pike_ErrorHandler';
+            set_error_handler(array($handler, 'dispatch'));
+        }
 
-        $path = isset($options['path']) ? $options['path'] : APPLICATION_PATH . '/../data/logs/php_errors.log';
-        $logger = new Zend_Log(new Zend_Log_Writer_Stream($path));
-        $logger->registerErrorHandler();
+        if (isset(Zend_Registry::get('config')->resources->log->php)) {
+            $config = Zend_Registry::get('config')->resources->log->php->toArray();
+
+            if (isset($config['writerParams']['permission'])) {
+                // Set the correct permissions on the log file
+                @chmod($config['writerParams']['stream'], octdec('0'. $config['writerParams']['permission']));
+            }
+
+            $logger = new Zend_Log();
+            $logger->addWriter($config);
+            $logger->registerErrorHandler();
+        }
     }
 }
