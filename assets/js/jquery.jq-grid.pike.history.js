5c21104799aa8b070fbee8c4808e152d170be761
pietervogelaar <pieter@pietervogelaar.nl>
2012-03-08 18:01:38 +0100

    Fixes several bugs when returned from another page

--- assets/js/jquery.jq-grid.pike.history.js
+++ assets/js/jquery.jq-grid.pike.history.js
@@ -28,13 +28,36 @@
     $.jgrid.pike = {};
 
     $.jgrid.pike.history = {
+        /**
+         * Params to store in hash
+         */
         params : ["page", "rowNum", "sortname", "sortorder"],
-        filters : true,
-        prefix : false,
-        defaults : [],
-        pushState : false,
-        reload : false,
 
+        /**
+         * Store filters in hash
+         */
+        filters : true,
+
+        /**
+         * Use prefix for params in hash
+         */
+        prefix : false,
+
+        /**
+         * Defaults
+         */
+        defaults : [],
+
+        /**
+         * Push state
+         */
+        pushState : false,
+
+        /**
+         * Returns the options for the grid with a new gridComplete callback function
+         *
+         * @param options
+         */
         getOptions : function(options) {
             var self = this;
 
@@ -49,19 +72,70 @@
             return options;
         },
 
+        /**
+         * Returns all grids from the current page
+         */
+        getGrids : function() {
+            return $('.ui-jqgrid-btable');
+        },
+
+        /**
+         * Sets the default params of a grid
+         */
+        setDefaults : function(grid) {
+            var defaults = [];
+            for (var i in this.params) {
+                defaults[this.params[i]] = grid.getGridParam(this.params[i]);
+            }
+            this.defaults[grid.getGridParam('id')] = defaults;
+        },
+
+        /**
+         * Grid complete callback function
+         */
         gridComplete : function() {
+            this.setFilterInputFields();
             this.buildHash();
         },
 
+        /**
+         * Sets the filter input fields with the values
+         */
+        setFilterInputFields : function() {
+            this.getGrids().each(function() {
+                var grid = $(this);
+                var filters = $.parseJSON(grid.getGridParam('postData').filters);
+                if (null !== filters) {
+                    grid.closest('.ui-jqgrid').find('.ui-search-toolbar input[id^="gs_"]')
+                    .each(function() {
+                        var field = $(this).attr('name');
+                        var value = '';
+                        for (var i in filters.rules) {
+                            if (field == filters.rules[i].field) {
+                                value = filters.rules[i].data;
+                                continue;
+                            }
+                        }
+                        $(this).val(value);
+                    });
+                }
+            });
+        },
+
+        /**
+         * Build the hash by adding the params and filters to the hash
+         */
         buildHash : function() {
             var self = this;
             var hash = $.bbq.getState();
-            $('.ui-jqgrid-btable').each(function() {
+            this.getGrids().each(function() {
                 var grid = $(this);
-                self.addParamsToHash(hash, grid);
+                if ('local' != grid.getGridParam('datatype')) {
+                    self.addParamsToHash(hash, grid);
 
-                if (self.filters) {
-                    self.addFiltersToHash(hash, grid);
+                    if (self.filters) {
+                        self.addFiltersToHash(hash, grid);
+                    }
                 }
             });
 
@@ -71,6 +145,12 @@
             }
         },
 
+        /**
+         * Adds the params to the hash
+         *
+         * @param hash
+         * @param grid
+         */
         addParamsToHash : function(hash, grid) {
             var id = grid.getGridParam('id');
             var i, param, value;
@@ -95,6 +175,12 @@
             }
         },
 
+        /**
+         * Adds the filters to the hash
+         *
+         * @param hash
+         * @param grid
+         */
         addFiltersToHash : function(hash, grid) {
             var id = grid.getGridParam('id');
             var prefix = (this.prefix ? id + '-' : '');
@@ -118,122 +204,186 @@
             }
         },
 
+        /**
+         * Bind the hashchange event to the window
+         */
         bindHashchangeEventToWindow : function() {
             var self = this;
-            $(window).bind('hashchange', function(event) {
+            $(window).bind('hashchange', function() {
                 self.hashchangeHandler();
             });
         },
 
         /**
-         * Hashchange handler
+         * Hashchange event handler
          */
         hashchangeHandler : function() {
             var self = this;
             var hash = $.bbq.getState();
 
-            $('.ui-jqgrid-btable').each(function() {
+            this.getGrids().each(function() {
                 var grid = $(this);
-                var id = grid.getGridParam('id');
-                var i, x, found, param, value, hashValue, params = [];
-                var prefix = (self.prefix ? id + '-' : '');
+                var params = new Array();
+                var reload = false;
 
-                // Handle params
-                for (i in self.params) {
-                    param = self.params[i];
-                    value = hash[(self.prefix ? id + '-' : '') + param] || self.defaults[id][param];
-                    if (grid.getGridParam(param) != value) {
-                        params[param] = value;
-                        self.reload = true;
+                reload = self.handleParams(params, hash, grid) || reload;
+
+                if (self.filters) {
+                    var filters = $.parseJSON(grid.getGridParam('postData').filters);
+                    var hashFilters = $.parseJSON(
+                        hash[(this.prefix ? grid.getGridParam('id') + '-' : '') + 'filters']);
+                    if (null === filters && null !== hashFilters) {
+                        filters = hashFilters;
+                    }
+
+                    reload = self.handleFilters(filters, hash, grid) || reload;
+
+                    if (filters) {
+                        params['postData'] = {
+                            filters : JSON.stringify(filters)
+                        };
                     }
                 }
 
-                var filters = $.parseJSON(grid.getGridParam('postData').filters);
-                var hashFilters = $.parseJSON(hash[prefix + 'filters']);
-
-                // Delete filters
-                if (null !== filters) {
-                    for (i in filters.rules) {
-                        found = false;
-                        if (null !== hashFilters) {
-                            for (x in hashFilters.rules) {
-                                if (filters.rules[i].field == hashFilters.rules[x].field) {
-                                    found = true;
-                                    continue;
-                                }
-                            }
-                        }
-                        if (!found) {
-                            grid.closest('.ui-jqgrid')
-                                .find('input[name="' + filters.rules[i].field + '"]')
-                                .val('');
-
-                            filters.rules.splice(i, 1);
-                            self.reload = true;
-                        }
-                    }
-                }
-
-                // Add filters
-                if (null !== hashFilters) {
-                    for (i in hashFilters.rules) {
-                        found = false;
-                        for (x in filters.rules) {
-                            if (hashFilters.rules[i].field == filters.rules[x].field) {
-                                value = filters.rules[x].data;
-                                hashValue = hashFilters.rules[i].data;
-                                if (value != hashValue) {
-                                    filters.rules[x].data = hashValue;
-
-                                    grid.closest('.ui-jqgrid')
-                                        .find('input[name="' + filters.rules[x].field + '"]')
-                                        .val(hashValue);
-
-                                    self.reload = true;
-                                }
-                                found = true;
-                                continue;
-                            }
-                        }
-                        if (!found) {
-                            filters.rules.push(hashFilters.rules[i]);
-
-                            grid.closest('.ui-jqgrid')
-                                .find('input[name="' + hashFilters.rules[i].field + '"]')
-                                .val(hashFilters.rules[i].data);
-
-                            self.reload = true;
-                        }
-                    }
-                }
-
-                // Add filters to postData
-                if (filters) {
-                    params['postData'] = {
-                        filters : JSON.stringify(filters)
-                    };
-                }
-
-                if (self.reload) {
-                    grid.setGridParam(params).trigger("reloadGrid");
-                    self.reload = false;
+                if (reload) {
+                    grid.setGridParam(params)
+                    .trigger("reloadGrid");
                 }
             });
         },
 
-        setDefaults : function(grid) {
-            var defaults = [];
-            for (var i in this.params) {
-                defaults[this.params[i]] = grid.getGridParam(this.params[i]);
+        /**
+         * Handles the params from the hash
+         *
+         * @param  params
+         * @param  hash
+         * @param  grid
+         * @return boolean  Reload needed?
+         */
+        handleParams : function(params, hash, grid) {
+            var id = grid.getGridParam('id');
+            var prefix = (this.prefix ? id + '-' : '');
+            var i, param, value, reload = false;
+
+            for (i in this.params) {
+                param = this.params[i];
+                value = hash[prefix + param] || this.defaults[id][param];
+                if (grid.getGridParam(param) != value) {
+                    params[param] = value;
+                    reload = true;
+                }
             }
-            this.defaults[grid.getGridParam('id')] = defaults;
+
+            return reload;
+        },
+
+        /**
+         * Handles the filters from the hash
+         *
+         * @param  filters
+         * @param  hash
+         * @param  grid
+         * @return boolean  Reload needed?
+         */
+        handleFilters : function(filters, hash, grid) {
+            var hashFilters = $.parseJSON(
+                hash[(this.prefix ? grid.getGridParam('id') + '-' : '') + 'filters']);
+            var reload = false;
+
+            if (null !== filters) {
+                reload = this.deleteFilters(filters, hashFilters) || reload;
+                if (null !== hashFilters) {
+                    reload = this.addFilters(filters, hashFilters) || reload;
+                }
+            }
+            return reload;
+        },
+
+        /**
+         * Deletes the filters that are not in the hash
+         *
+         * @param  filters
+         * @param  hashFilters
+         * @return boolean  Reload needed?
+         */
+        deleteFilters : function(filters, hashFilters) {
+            var i, x, found, reload = false;
+
+            for (i in filters.rules) {
+                found = false;
+                if (null !== hashFilters) {
+                    for (x in hashFilters.rules) {
+                        if (filters.rules[i].field == hashFilters.rules[x].field) {
+                            found = true;
+                            continue;
+                        }
+                    }
+                }
+                if (!found) {
+                    filters.rules.splice(i, 1);
+                    reload = true;
+                }
+            }
+
+            return reload;
+        },
+
+        /**
+         * Adds the filters from the hash that are not already in the filters object
+         *
+         * @param  filters
+         * @param  hashFilters
+         * @return boolean  Reload needed?
+         */
+        addFilters : function(filters, hashFilters) {
+            var i, x, value, hashValue, found, reload = false;
+
+            for (i in hashFilters.rules) {
+                found = false;
+                for (x in filters.rules) {
+                    if (hashFilters.rules[i].field == filters.rules[x].field) {
+                        value = filters.rules[x].data;
+                        hashValue = hashFilters.rules[i].data;
+                        if (value != hashValue) {
+                            filters.rules[x].data = hashValue;
+                            reload = true;
+                        }
+                        found = true;
+                        continue;
+                    }
+                }
+                if (!found) {
+                    filters.rules.push(hashFilters.rules[i]);
+                    reload = true;
+                }
+            }
+
+            return reload;
         }
     };
 
+    /**
+     * jqGridHistory function
+     *
+     * @param options
+     */
     $.fn.jqGridHistory = function(options) {
+        var datatype = options.datatype;
+        if ('' != datatype) {
+            options.datatype = 'local';
+        }
+
         this.jqGrid($.jgrid.pike.history.getOptions(options));
         $.jgrid.pike.history.bindHashchangeEventToWindow();
         $.jgrid.pike.history.setDefaults(this);
+
+        if ('' != datatype) {
+            this.setGridParam({
+                datatype: datatype
+            });
+        }
+
+        $.jgrid.pike.history.hashchangeHandler();
         return this;
     };
 })(jQuery);
