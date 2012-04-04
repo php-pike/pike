dc59522b67356f11ec8449282d0c106dd96a42d5
pietervogelaar <pieter@pietervogelaar.nl>
2012-04-04 08:52:45 +0200

    Added license doc block

--- Validate/DateCompare.php
+++ Validate/DateCompare.php
@@ -1,5 +1,25 @@
 <?php
 /**
+ * Copyright (C) 2011 by Pieter Vogelaar (pietervogelaar.nl) and Kees Schepers (keesschepers.nl)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
  * @category   PiKe
  * @copyright  Copyright (C) 2011 by Pieter Vogelaar (pietervogelaar.nl) and Kees Schepers (keesschepers.nl)
  * @package    Validate
@@ -55,10 +75,12 @@ class Pike_Validate_DateCompare extends Zend_Validate_Abstract
         self::INVALID_VALUE => "The date '%value%' is not valid",
         self::INVALID_TOKEN => "The date '%token%' is not valid"
     );
+    
     /**
      * @var array
      */
     protected $_messageVariables = array('token' => '_tokenValue');
+    
     /**
      * Token to validate against
      *
@@ -252,4 +274,3 @@ class Pike_Validate_DateCompare extends Zend_Validate_Abstract
         return true;
     }
 }
-
--- Validate/Url.php
+++ Validate/Url.php
@@ -1,15 +1,34 @@
 <?php
 /**
+ * Copyright (C) 2011 by Pieter Vogelaar (pietervogelaar.nl) and Kees Schepers (keesschepers.nl)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
  * @category   PiKe
  * @copyright  Copyright (C) 2011 by Pieter Vogelaar (pietervogelaar.nl) and Kees Schepers (keesschepers.nl)
  * @package    Validate
  * @license    MIT
  */
 
-/** 
+/**
+ * Tests if the specified value is a valid url
  *
- * Test if given value is a valid url
- * 
  * @category   PiKe
  * @copyright  Copyright (C) 2011 by Pieter Vogelaar (pietervogelaar.nl) and Kees Schepers (keesschepers.nl)
  * @package    Validate
@@ -19,12 +38,14 @@ class Pike_Validate_Url extends Zend_Validate_Abstract
 {
     /**
      * Error codes
+     * 
      * @const string
      */
     const INVALID_URL = 'invalidUrl';
 
     /**
      * Error messages
+     * 
      * @var array
      */
     protected $_messageTemplates = array(
@@ -35,10 +56,10 @@ class Pike_Validate_Url extends Zend_Validate_Abstract
      * Defined by Zend_Validate_Interface
      *
      * Returns true if and only if the $value is a valid url that starts with http(s)://
-     * and the hostname is a valid TLD
+     * and the hostname is a valid TLD.
      *
-     * @param string $value
-     * @throws Zend_Validate_Exception if a fatal error occurs for validation process
+     * @param  string $value
+     * @throws Zend_Validate_Exception If a fatal error occurs for validation process
      * @return boolean
      */
     public function isValid($value)
@@ -49,7 +70,8 @@ class Pike_Validate_Url extends Zend_Validate_Abstract
         }
 
         $this->_setValue($value);
-        //get a Zend_Uri_Http object for our URL, this will only accept http(s) schemes
+        
+        // Get a Zend_Uri_Http object for our URL, this will only accept http(s) schemes
         try {
             $uriHttp = Zend_Uri_Http::fromString($value);
         } catch (Zend_Uri_Exception $e) {
@@ -57,14 +79,14 @@ class Pike_Validate_Url extends Zend_Validate_Abstract
             return false;
         }
 
-        //if we have a valid URI then we check the hostname for valid TLDs, and not local urls
+        // If we have a valid URI then we check the hostname for valid TLDs, and not local urls
         $hostnameValidator = new Zend_Validate_Hostname(Zend_Validate_Hostname::ALLOW_DNS); //do not allow local hostnames, this is the default
 
         if (!$hostnameValidator->isValid($uriHttp->getHost())) {
             $this->_error(self::INVALID_URL);
             return false;
         }
+        
         return true;
     }
-
 }
