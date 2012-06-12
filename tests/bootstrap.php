c5a33c8e123700e6c14e9dc09c2e8d21e718fdde
Kees Schepers <kees@kees-laptop.(none)>
2012-06-12 08:14:48 +0200

    First actual unit-tests for Pike_Debug

---
+++ tests/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+if(false === is_dir(__DIR__ . '/../vendor')) {
+    throw new RuntimeException('You should run: php composer.phar install first'
+            . ' to load mandatory dependencies.');
+}
+
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(__DIR__ . '/./application'));
+
+define('APPLICATION_ENV', 'testing');
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(__DIR__ . '/../vendor'),
+    realpath(__DIR__ . '/../lib'),
+    get_include_path(),
+)));
+
+include_once 'autoload.php'; //composer
+$loader = Zend_Loader_Autoloader::getInstance();
+$loader->registerNamespace('Pike');
--- tests/lib/Pike/DebugTest.php
+++ tests/lib/Pike/DebugTest.php
@@ -1,6 +1,90 @@
 <?php
+
 class DebugTestCase extends PHPUnit_Framework_TestCase {
+
     public function testDump() {
-        $this->assertTrue(true);
+        $dump = Pike_Debug::dump(array(
+                    'test' => 'test123',
+                    0 => new stdClass()
+                        ), 1, 'some test', false);
+
+        $shouldBe = <<<EOS
+
+some test 
+Array
+(
+    [test] => 'test123'
+    [0] => stdClass(...)
+)
+
+EOS;
+
+        $this->assertEquals($shouldBe, $dump);
     }
+
+    public function testDepth() {
+        $dump = Pike_Debug::dump(array(
+                    'test' => array(
+                        'level1' => array(
+                            'level2' => array(
+                                'level3' => array(
+                                    'level4' => 'yay!'
+                                )
+                            )
+                    )),
+                    'someLevel1' => 'Is it so?',
+                    'TakeItDeeper' => array(
+                        'Yeah, we are one 2!' => 'smooth',
+                        'someValue'
+                        )), 2, null, false);
+
+        $shouldBe = <<<EOS
+
+
+Array
+(
+    [test] => Array
+    (
+        [level1] => Array(...)
+    )
+    [someLevel1] => 'Is it so?'
+    [TakeItDeeper] => Array
+    (
+        [Yeah, we are one 2!] => 'smooth'
+        [0] => 'someValue'
+    )
+)
+
+EOS;
+
+        $this->assertEquals($shouldBe, $dump);
+    }
+
+    /**
+     * Pike_Debug should print $dump variable directly to php://out unless    
+     * a third parameter false is given.
+     */
+    public function testOutputBuffer() {
+        ob_start();
+        
+        $dump = Pike_Debug::dump(array(
+                    'test' => 'test123',
+                    0 => new stdClass()
+                        ), 1, 'dump test');
+
+        $shouldBe = <<<EOS
+
+dump test 
+Array
+(
+    [test] => 'test123'
+    [0] => stdClass(...)
+)
+
+EOS;
+        $dump = ob_get_clean();
+        
+        $this->assertEquals($shouldBe, $dump);
+    }
+
 }
